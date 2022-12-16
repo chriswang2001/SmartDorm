4f4d8ff6b3bd8a5c973dd6b254765bcc3c88b1e3
haozhou.wong <haozhou.wong@gmail.com>
2022-12-16 21:52:53 +0800

    OTA完善

---
+++ esp32/dorm1/dorm1.h
@@ -0,0 +1,8 @@
+#pragma once
+
+//房间的wifi名和密码
+#define ssid "寻隐者不遇"
+#define password "8728wang3325zhang"
+
+//OTA网址域名 http://dorm1.local
+#define host "dorm1"
--- esp32/dorm1/dorm1.ino
+++ esp32/dorm1/dorm1.ino
@@ -3,58 +3,9 @@
 #include <WebServer.h>
 #include <ESPmDNS.h>
 #include <Update.h>
-
-const char* host = "esp32";
-const char* ssid = "不要联网!不要联网!!";
-const char* password = "87283325wangyangli";
+#include "dorm1.h"
 
 WebServer server(80);
-
-/*
- * Login page
- */
-
-const char* loginIndex =
- "<form name='loginForm'>"
-    "<table width='20%' bgcolor='A09F9F' align='center'>"
-        "<tr>"
-            "<td colspan=2>"
-                "<center><font size=4><b>ESP32 Login Page</b></font></center>"
-                "<br>"
-            "</td>"
-            "<br>"
-            "<br>"
-        "</tr>"
-        "<tr>"
-             "<td>Username:</td>"
-             "<td><input type='text' size=25 name='userid'><br></td>"
-        "</tr>"
-        "<br>"
-        "<br>"
-        "<tr>"
-            "<td>Password:</td>"
-            "<td><input type='Password' size=25 name='pwd'><br></td>"
-            "<br>"
-            "<br>"
-        "</tr>"
-        "<tr>"
-            "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
-        "</tr>"
-    "</table>"
-"</form>"
-"<script>"
-    "function check(form)"
-    "{"
-    "if(form.userid.value=='admin' && form.pwd.value=='admin')"
-    "{"
-    "window.open('/serverIndex')"
-    "}"
-    "else"
-    "{"
-    " alert('Error Password or Username')/*displays error message*/"
-    "}"
-    "}"
-"</script>";
 
 /*
  * Server Index Page
@@ -119,7 +70,7 @@ void setup(void) {
   Serial.println(WiFi.localIP());
 
   /*use mdns for host name resolution*/
-  if (!MDNS.begin(host)) { //http://esp32.local
+  if (!MDNS.begin(host)) {
     Serial.println("Error setting up MDNS responder!");
     while (1) {
       delay(1000);
@@ -128,10 +79,6 @@ void setup(void) {
   Serial.println("mDNS responder started");
   /*return index page which is stored in serverIndex */
   server.on("/", HTTP_GET, []() {
-    server.sendHeader("Connection", "close");
-    server.send(200, "text/html", loginIndex);
-  });
-  server.on("/serverIndex", HTTP_GET, []() {
     server.sendHeader("Connection", "close");
     server.send(200, "text/html", serverIndex);
   });
